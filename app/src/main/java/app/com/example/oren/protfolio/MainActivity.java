6f5017168cb3b5c2459c4d59981a57d379db7ff7
orenshamun <oren.shamun@gmail.com>
2015-09-04 17:26:51 +0300

    Add files - firs

---
+++ app/src/main/java/app/com/example/oren/protfolio/MainActivity.java
@@ -0,0 +1,75 @@
+package app.com.example.oren.protfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //region Click listeners
+    public void onClickBtn1(View view){
+        showToast("This button will launch my Movies App");
+    }
+
+    public void onClickBtn2(View view){
+        showToast("This button will launch my Scores App");
+    }
+
+    public void onClickBtn3(View view){
+        showToast("This button will launch my Library App");
+    }
+
+    public void onClickBtn4(View view){
+        showToast("This button will launch my Build it bigger App");
+    }
+
+    public void onClickBtn5(View view){
+        showToast("This button will launch my XYZ reader App");
+    }
+
+    public void onClickBtn6(View view){
+        showToast("This button will launch my Capstone App");
+    }
+    //endregion
+
+    private void showToast(String msg){
+        Context context = getApplicationContext();
+        CharSequence text = msg;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
